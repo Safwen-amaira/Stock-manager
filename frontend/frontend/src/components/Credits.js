5fa35365d6e60b81e5f47e32277097dea2eb80c2
Safwen-amaira <amairasafwen@icloud.com>
2024-09-13 16:28:16 +0100

    adding credits

--- frontend/frontend/src/components/Credits.js
+++ frontend/frontend/src/components/Credits.js
@@ -1,24 +1,28 @@
-import React, { Component, useState } from 'react'
-import None from './None';
+import React, { Component, useState,useEffect } from 'react'
 import axios from 'axios'
+
 
 const Credits = ()=>{
 
   const [credits,setCredits]= useState([]);
-  const fetchCredits = () =>{
-     try {
-      
-      const response = axios.get('http://localhost:8000/')
-      setCredits(response.data)
-      }
-     catch (Exception ) {
-      console.log ("error = > " + Exception)
-     }
-  }
+  
+  
+  useEffect(() => {
+    const fetchCredits = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/api/credits/getall');
+        console.log(response.data); // Check what data is being returned
+        setCredits(response.data);
 
+      } catch (error) {
+        console.error('Failed to fetch sells:', error);
+      }
+    };
+    fetchCredits()
+  },[]);
     return (
       <div>
-
+            <h1>Testing API </h1>
       </div>
     )
   
